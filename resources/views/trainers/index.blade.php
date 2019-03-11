49c0ca5889f933b3a0c0d791631581a30429a410
Jahn <jorgechido75@hotmail.com>
2019-03-10 23:08:09 -0600

    video 19

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -7,11 +7,11 @@
 	<div class="row">
 		@foreach($trainers as $trainer)
 			<div class="col-sm">
-				<div class="card" style="width: 18rem;">	
-				<img class="card-img-top" src="/images/{{$trainer->avatar}}">				
+				<div class="card text-center" style="width: 18rem; margin-top: 50px">
+					<img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px" class="card-img-top rounded-circle mx-auto d-block" src="/images/{{$trainer->avatar}}">
   					<div class="card-body">
     					<h5 class="card-title">{{$trainer->name}}</h5>
-    					<p class="card-text">Descripcion descriptiva</p>
+    					<p class="card-text">Descripcion pedorra</p>
     					<a href="#" class="btn btn-primary">Ver más...</a>
   					</div>
   				</div>
