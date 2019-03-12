74c535f2428be5511a400bce195d3ff89021e99a
Jahn <jorgechido75@hotmail.com>
2019-03-11 23:07:08 -0600

    video 39

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::get('/name/{name}/apellido/{apellido?}', function($name,$apellido=null){
 Route::get('prueba/{name}','PruebaController@prueba');
 
 Route::resource('trainers', 'TrainerController');
+
+Route::resource('pokemons', 'PokemonController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
