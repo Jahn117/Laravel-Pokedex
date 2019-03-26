e688d163adbd8ad8f578a51d4c986079e148349c
Jahn <jorgechido75@hotmail.com>
2019-03-26 14:26:08 -0600

    video 44

--- resources/views/pokemons/index.blade.php
+++ resources/views/pokemons/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
+	<add-pokemon-btn></add-pokemon-btn>
 	<pokemons-component></pokemons-component>
+	<create-form-component></create-form-component>
 
 @endsection
